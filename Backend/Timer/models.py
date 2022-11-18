1e7e463ba48cf38b9fc1d3bcdfbb51201dee296a
Vladyslav Nosov <vladorthebest@gmail.com>
2022-11-18 16:49:00 +0100

    Create Model Activity

--- Backend/Timer/models.py
+++ Backend/Timer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Activity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    time_start = models.DateTimeField(auto_now=True)
+    duration = models.DurationField(null=True, blank=True)
+    activity_status = models.BooleanField(default=True)

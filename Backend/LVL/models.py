1d7a974515c2398d65cc313718d0ae50a8f88c6e
Vladyslav Nosov <vladorthebest@gmail.com>
2023-01-26 14:48:36 +0100

    Add signals for Level model

--- Backend/LVL/models.py
+++ Backend/LVL/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 from django.contrib.auth.models import User
-
+from Timer.models import Activity
 
 class Level(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,3 +13,16 @@ class Level(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def user_created_handler(sender, instance, created, *args, **kwargs):
+    if created:
+        Level.objects.create(user=instance)
+    
+@receiver(post_save, sender=Activity)
+def user_created_handler(sender, instance, created, *args, **kwargs):
+    if not created:
+        instance_profile = Level.objects.get(user=instance.user)
+        instance_profile.exp += instance.exp
+        instance_profile.save()

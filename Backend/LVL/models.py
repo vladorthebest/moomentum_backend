87f9fd5b6d4cf35bd352490c36d284d42e9dc36b
Vladyslav Nosov <vladorthebest@gmail.com>
2023-01-26 14:47:28 +0100

    Create Level model

--- Backend/LVL/models.py
+++ Backend/LVL/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+
+from django.contrib.auth.models import User
+
+
+class Level(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    lvl = models.IntegerField(default=1)
+    exp = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username

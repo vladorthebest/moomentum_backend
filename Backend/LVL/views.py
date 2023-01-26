2b7450b68bb2328e5ed2dcd22b2ea96a988872ee
Vladyslav Nosov <vladorthebest@gmail.com>
2023-01-26 15:03:09 +0100

    Create LevelAPI view

--- Backend/LVL/views.py
+++ Backend/LVL/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import LevelSerializer
+from .models import Level
+
+
+class LevelAPI(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        try:
+            return self.queryset.get(user=self.request.user.id)
+        except ObjectDoesNotExist:
+            return False
